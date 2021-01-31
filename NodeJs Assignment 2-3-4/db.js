32d359cabed27129eab76f922e51d18053bcba0e
Dhrumit Soni <sonidhrumit999@gmail.com>
2021-02-01 01:35:04 +0530

    NodeJS Assignment 3 and 4

--- NodeJs Assignment 2-3-4/db.js
+++ NodeJs Assignment 2-3-4/db.js
@@ -1,5 +1,8 @@
-// Connecting to PostgreSQL Database
+// Requirements
+const multer = require("multer");
 const Pool = require("pg").Pool;
+
+// Connecting to PostgreSQL Database
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
@@ -9,16 +12,17 @@ const pool = new Pool({
 });
 
 // API functions for Routes
-
 // GET: /cars
 const getCars = (req, resp) => {
   try {
     pool
       .query(
         `
-          SELECT c.id AS "Id", c.name AS "Name", mo.name AS "Model", mk.name AS "Make" 
-              FROM cars c LEFT JOIN models mo ON c.model_id = mo.id 
-              LEFT JOIN make mk on c.make_id = mk.id ORDER BY c.id ASC
+        SELECT c.id, c.name, mo.name AS "model", mk.name AS "make",
+          (SELECT JSON_AGG(ROW_TO_JSON(x)) AS "images" FROM 
+            (SELECT ci.path FROM carimages ci WHERE ci.car_id = c.id) x) 
+          FROM cars c LEFT JOIN models mo ON c.model_id = mo.id 
+          LEFT JOIN make mk ON c.make_id = mk.id ORDER BY c.id ASC
         `
       )
       .then((result) => {
@@ -33,7 +37,7 @@ const getCars = (req, resp) => {
 };
 
 // GET: /car/:id
-const getCarById = (req, resp) => {
+const getCarById = async (req, resp) => {
   try {
     const id = parseInt(req.params.id);
     if (isNaN(id)) {
@@ -41,26 +45,22 @@ const getCarById = (req, resp) => {
         error: "Invalid Car Id",
       });
     } else {
-      pool
-        .query(
-          `
-            SELECT c.id AS "Id", c.name AS "Name", mo.name AS "Model", mk.name AS "Make" 
-                FROM cars c LEFT JOIN models mo ON c.model_id = mo.id 
-                LEFT JOIN make mk on c.make_id = mk.id WHERE c.id = ${id} ORDER BY c.id ASC
-          `
-        )
-        .then((result) => {
-          if (result.rows.length == 0) {
-            resp.status(400).json({
-              error: "Invalid Car Id",
-            });
-          } else {
-            resp.status(200).json(result.rows[0]);
-          }
-        })
-        .catch((err) => {
-          resp.status(500).json(err);
+      const result = await pool.query(
+        `        
+        SELECT c.id, c.name, mo.name AS "model", mk.name AS "make",
+          (SELECT JSON_AGG(ROW_TO_JSON(x)) AS "images" FROM 
+            (SELECT ci.path FROM carimages ci WHERE ci.car_id = c.id) x) 
+          FROM cars c LEFT JOIN models mo ON c.model_id = mo.id 
+          LEFT JOIN make mk ON c.make_id = mk.id WHERE c.id = ${id} 
+        `
+      );
+      if (result.rowCount < 1) {
+        resp.status(400).json({
+          error: "Invalid Car Id",
         });
+      } else {
+        resp.status(200).json(result.rows[0]);
+      }
     }
   } catch (err) {
     resp.status(500).json(err);
@@ -68,7 +68,7 @@ const getCarById = (req, resp) => {
 };
 
 // POST: /car
-const postCar = (req, resp) => {
+const postCar = async (req, resp) => {
   try {
     if (req.body == null || req.body == undefined) {
       // req.body was empty so no data was received
@@ -77,9 +77,18 @@ const postCar = (req, resp) => {
       });
     } else {
       const car = req.body;
-      validateCar(car);
-      const model = checkModel(car);
-      checkMake(car);
+      checkObject(car);
+      await checkName(car.name);
+      const model = await checkModel(car.model);
+      const make = await checkMake(car.make);
+      const insertResult = await pool.query(
+        `INSERT INTO cars (name, model_id, make_id) VALUES ('${car.name}', '${model}', '${make}') RETURNING *`
+      );
+      if (insertResult.rowCount == 1) {
+        resp.status(200).json(insertResult.rows[0]);
+      } else {
+        throw Error("Error in saving the car");
+      }
     }
   } catch (err) {
     resp.status(500).json({
@@ -88,8 +97,137 @@ const postCar = (req, resp) => {
   }
 };
 
+// POST: /car/:id/image
+const postCarImage = async (req, resp) => {
+  try {
+    const id = parseInt(req.params.id);
+    if (!isNaN(id)) {
+      const insertResult = await pool.query(
+        `INSERT INTO carimages (car_id, path) 
+          VALUES (${id}, 'http:localhost:3000/images/${req.file.filename}') RETURNING *`
+      );
+      if (insertResult.rowCount < 1) {
+        throw Error("Error in saving image");
+      } else {
+        resp.status(201).json(insertResult.rows[0]);
+      }
+    }
+  } catch (err) {
+    resp.status(500).json({
+      error: err.message,
+    });
+  }
+};
+
+//PUT: /car/:id
+const putCar = async (req, resp) => {
+  try {
+    const id = parseInt(req.params.id);
+    if (!isNaN(id)) {
+      if (req.body == null || req.body == undefined) {
+        // req.body was empty so no data was received
+        resp.status(400).json({
+          error: "No data to update !!",
+        });
+      } else {
+        // Retrieve old details
+        const oldCar = await fetchCar(id);
+
+        // Get new details from req.body
+        const updateDetails = req.body;
+
+        // Check of name is passed. If it is not passed, keep it null.
+        const name =
+          updateDetails.name == null ||
+          updateDetails.name == undefined ||
+          updateDetails.name == ""
+            ? null
+            : updateDetails.name;
+
+        // If it is passed check if it exists or not.
+        // If it exists, throw error, else return.
+        await checkName(name);
+
+        // Check of model is passed. If it is not passed, keep it null.
+        // If it is passed check if it exists or not.
+        // It it exists, return id, else make a new entry and return new id.
+        const model =
+          updateDetails.model == null ||
+          updateDetails.model == undefined ||
+          updateDetails.model == ""
+            ? null
+            : await checkModel(updateDetails.model);
+
+        // Check of make is passed. If it is not passed, keep it null.
+        // If it is passed check if it exists or not.
+        // It it exists, return id, else make a new entry and return new id.
+        const make =
+          updateDetails.make == null ||
+          updateDetails.make == undefined ||
+          updateDetails.make == ""
+            ? null
+            : await checkMake(updateDetails.make);
+
+        // Make a new car object to update
+        const newCar = {
+          id: oldCar.id,
+          name: name == null ? oldCar.name : name,
+          model: model == null ? oldCar.model : model,
+          make: make == null ? oldCar.make : model,
+        };
+
+        const result = await pool.query(
+          `UPDATE cars SET name = '${newCar.name}', model_id = ${newCar.model}, make_id = ${newCar.make} WHERE id = ${newCar.id} RETURNING *`
+        );
+        if (result.rowCount < 1) {
+          throw Error("Error in updating car details");
+        } else {
+          resp.status(200).json(result.rows[0]);
+        }
+      }
+    } else {
+      resp.status(400).json({
+        error: "Invalid Car Id",
+      });
+    }
+  } catch (err) {
+    resp.status(500).json({
+      error: err.message,
+    });
+  }
+};
+
+// DELETE: /car/:id
+const deleteCar = async (req, resp) => {
+  try {
+    const id = parseInt(req.params.id);
+    if (!isNaN(id)) {
+      if (await checkCar(id)) {
+        const deleteResult = await pool.query(
+          `DELETE FROM cars WHERE cars.id = ${id} RETURNING *`
+        );
+        if (deleteResult.rowCount < 1) {
+          throw Error("Error in deleting the car");
+        } else {
+          resp.status(200).json(deleteResult.rows[0]);
+        }
+      } else {
+        throw Error("Invalid Car Id");
+      }
+    } else {
+      throw Error("Invalid car id");
+    }
+  } catch (err) {
+    resp.status(500).json({
+      error: err.message,
+    });
+  }
+};
+
+// Helper Functions
+
 // Validating Car Object
-const validateCar = (car) => {
+const checkObject = (car) => {
   if (
     car != null &&
     car.name != null &&
@@ -104,34 +242,95 @@ const validateCar = (car) => {
   throw Error("Invalid Car Details");
 };
 
-// Checking if Model exists
-const checkModel = async (car) => {
+// Checking if Car record exists
+const checkCar = async (id) => {
   try {
-    const result = await pool.query(
-      `SELECT id FROM models WHERE models.name ILIKE '${car.model}'`
+    const selectResult = await pool.query(
+      `SELECT * FROM cars WHERE cars.id = ${id}`
     );
-    if (result.rowCount < 1) {
-      const newModel = await pool.query(
-        `INSERT INTO models (name) VALUES ('${car.model}')`
-      );
-      return newModel;
+    if (selectResult.rowCount < 1) {
+      return false;
     } else {
-      return result.rows[0];
+      return true;
     }
   } catch (err) {
-    resp.status(500).json({
-      error: err.message,
-    });
+    throw Error(err.message);
+  }
+};
+
+// Checking if CarName already taken
+const checkName = async (name) => {
+  try {
+    const selectResult = await pool.query(
+      `SELECT * FROM cars WHERE cars.name ILIKE '${name}'`
+    );
+    if (selectResult.rowCount < 1) {
+      return;
+    } else {
+      throw Error("A car with the same name already exists !!!");
+    }
+  } catch (err) {
+    throw Error(err.message);
+  }
+};
+
+// Checking if Model exists
+const checkModel = async (model) => {
+  try {
+    const selectResult = await pool.query(
+      `SELECT id FROM models WHERE models.name ILIKE '${model}'`
+    );
+    if (selectResult.rowCount < 1) {
+      const insertResult = await pool.query(
+        `INSERT INTO models (name) VALUES ('${model}') RETURNING id`
+      );
+      return insertResult.rows[0].id;
+    } else {
+      return selectResult.rows[0].id;
+    }
+  } catch (err) {
+    throw Error(err.message);
   }
 };
 
 // Checking if Make exists
-const checkMake = (car) => {
-  console.log(car.make);
+const checkMake = async (make) => {
+  try {
+    const selectResult = await pool.query(
+      `SELECT id FROM make WHERE make.name ILIKE '${make}'`
+    );
+    if (selectResult.rowCount < 1) {
+      const insertResult = await pool.query(
+        `INSERT INTO make (name) VALUES ('${make}') RETURNING id`
+      );
+      return insertResult.rows[0].id;
+    } else {
+      return selectResult.rows[0].id;
+    }
+  } catch (err) {
+    throw Error(err.message);
+  }
+};
+
+// Getting car details by id
+const fetchCar = async (id) => {
+  const result = await pool.query(
+    `SELECT c.id AS "id", c.name AS "name", c.model_id AS "model", c.make_id AS "make" 
+      FROM cars c WHERE c.id = ${id} 
+      `
+  );
+  if (result.rowCount < 1) {
+    throw Error("Invalid Car Id");
+  } else {
+    return result.rows[0];
+  }
 };
 
 module.exports = {
   getCars,
   getCarById,
   postCar,
+  postCarImage,
+  putCar,
+  deleteCar,
 };
--- NodeJs Assignment 2-3-4/index.js
+++ NodeJs Assignment 2-3-4/index.js
@@ -1,4 +1,7 @@
+// Requirments
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
 const db = require("./db");
 
 // Initializing Express
@@ -9,16 +12,43 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Configuring Multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    let filetype = file.mimetype.replace("image/", "");
+    cb(null, "image_" + Date.now() + "." + filetype);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  },
+});
+
 // Setting routes
 app.get("/", (req, resp) => {
   resp.status(200).json({
     status: `Server is up and running on port ${port}`,
   });
 });
-
 app.get("/cars", db.getCars);
 app.get("/car/:id", db.getCarById);
 app.post("/car", db.postCar);
+app.post("/car/:id/image", upload.single("image"), db.postCarImage);
+app.put("/car/:id", db.putCar);
+app.delete("/car/:id", db.deleteCar);
+
+// Serving images
+app.use("/images", express.static(path.join(__dirname, "uploads")));
 
 // Starting server
 app.listen(port, () => {
